44f15631daa8a043127d7ef1735d0c34874c8cf2
ADMINIOI <original264641@gmail.com>
2025-09-28 23:36:24 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,35 @@
+const CACHE_NAME = 'axisbox-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/script.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('axisbox-cache').then(cache => {
-      return cache.addAll([
-        '/AxisBox.github.io/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
